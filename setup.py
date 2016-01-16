57054f2be71659a9e7dfa6e87ebc84504a5fe99d
Tim Zehta <tim@clockwork.net>
2016-01-16 06:27:52 -0600

    Version 1.03. Added trove classifiers.

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+"""gacli setup
+"""
+
+# Standard library
+from __future__ import absolute_import, division, print_function
 try:
     # Third-party
     from setuptools import setup
@@ -6,23 +12,32 @@ except ImportError:
     # Standard library
     from distutils.core import setup
 
-with open('README.rst') as file:
-    long_description = file.read()
+with open("README.rst", "rb") as file:
+    long_description = file.read().decode("utf-8")
+
+classifiers = ["Environment :: Console",
+               "Intended Audience :: System Administrators",
+               "Intended Audience :: Developers",
+               "License :: OSI Approved :: MIT License",
+               "Natural Language :: English",
+               "Operating System :: MacOS :: MacOS X",
+               "Operating System :: POSIX :: Linux",
+               "Programming Language :: Python :: 2",
+               "Programming Language :: Python :: 2.7",
+               "Programming Language :: Python :: 3",
+               "Programming Language :: Python :: 3.5",
+               "Programming Language :: Python :: Implementation :: CPython",
+               "Topic :: Security",
+               "Topic :: Utilities"]
 
 setup(author="Timid Robot Zehta",
       author_email="tim@clockwork.net",
-      classifiers=["Environment :: Console",
-                   "Intended Audience :: System Administrators",
-                   "Intended Audience :: Developers",
-                   "License :: OSI Approved :: MIT License",
-                   "Topic :: Security",
-                   "Topic :: Utilities",
-                   ],
+      classifiers=classifiers,
       description="Google Authentication command line tools",
       install_requires=["onetimepass"],
       long_description=long_description,
       name="gacli",
       scripts=["ga"],
       url="https://github.com/ClockworkNet/gacli/",
-      version="1.0.2",
+      version="1.0.3",
       )
